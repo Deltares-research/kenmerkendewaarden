64b40009f536d173db6ce821813da347a908c45d
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-06-12 10:02:45 +0200

    havengetallen extra test

--- tests/test_havengetallen.py
+++ tests/test_havengetallen.py
@@ -21,6 +21,19 @@ def test_havengetallen(df_ext_12_2010):
 
 
 @pytest.mark.unittest
+def test_calc_HWLW_culmhr_summary_tidalcoeff(df_ext_12_2010):
+    """
+    this function is not used, so might be removed in the future
+    still good to add test for now
+    """
+    df_tidalcoeff = kw.havengetallen.calc_HWLW_culmhr_summary_tidalcoeff(df_ext_12_2010)
+    expected = np.array([[ 0.87 , -0.575,  1.42 ],
+       [ 1.16 , -0.6  ,  1.75 ],
+       [ 1.34 , -0.775,  2.07 ]])
+    assert np.allclose(df_tidalcoeff, expected)
+
+
+@pytest.mark.unittest
 def test_plot_HWLW_pertimeclass(df_ext_12_2010):
     df_havengetallen, data_pd_hwlw = kw.calc_havengetallen(df_ext=df_ext_12_2010, return_df_ext=True)
     kw.plot_HWLW_pertimeclass(df_ext=data_pd_hwlw, df_havengetallen=df_havengetallen)
