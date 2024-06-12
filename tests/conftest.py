e146aa51b2ae090fdf5985b38cdf21a2ee23d06f
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-06-12 09:15:47 +0200

    made fixtures scope sessioned where possible

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,49 +13,49 @@ dir_tests = os.path.dirname(__file__) #F9 doesnt work, only F5 (F5 also only met
 dir_testdata = os.path.join(dir_tests,'testdata')
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_meas():
     file_dia_ts = os.path.join(dir_testdata, "HOEK_KW.dia")
     df_meas = hatyan.read_dia(file_dia_ts)
     return df_meas
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_meas_2010(df_meas):
     df_meas_2010 = df_meas.loc["2010":"2010"]
     assert len(df_meas_2010) == 52560
     return df_meas_2010
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_meas_2010_2014(df_meas):
     df_meas_2010_2014 = df_meas.loc["2010":"2014"]
     assert len(df_meas_2010_2014) == 262944
     return df_meas_2010_2014
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_ext():
     file_dia_ext = os.path.join(dir_testdata, "HOEKVHLD_ext.dia")
     df_ext = hatyan.read_dia(file_dia_ext, station="HOEKVHLD", block_ids="allstation")
     return df_ext
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_ext_2010(df_ext):
     df_ext_2010 = df_ext.loc["2010":"2010"]
     assert len(df_ext_2010) == 1863
     return df_ext_2010
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_ext_2010_2014(df_ext):
     df_ext_2010_2014 = df_ext.loc["2010":"2014"]
     assert len(df_ext_2010_2014) == 9881
     return df_ext_2010_2014
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_ext_12_2010(df_ext):
     df_ext_sel = df_ext.loc["2009-12-28":"2011-01-03"]
     df_ext_12 = hatyan.calc_HWLW12345to12(df_ext_sel)
@@ -64,7 +64,7 @@ def df_ext_12_2010(df_ext):
     return df_ext_12_2010
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_ext_12_2010_2014(df_ext):
     df_ext_sel = df_ext.loc["2009-12-28":"2015-01-03"]
     df_ext_12 = hatyan.calc_HWLW12345to12(df_ext_sel)
@@ -73,12 +73,13 @@ def df_ext_12_2010_2014(df_ext):
     return df_ext_12_2010_2014
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def df_components_2010(df_meas_2010):
     df_components_2010 = hatyan.analysis(df_meas_2010, const_list="year")
     return df_components_2010
 
 
+# adding scope will raise "Failed: ScopeMismatch: You tried to access the function scoped fixture tmp_path with a session scoped request objec"
 @pytest.fixture
 def dir_meas(tmp_path):
     dir_meas = tmp_path
@@ -94,6 +95,7 @@ def dir_meas(tmp_path):
     return dir_meas
 
 
+# adding scope will raise "Failed: ScopeMismatch: You tried to access the function scoped fixture tmp_path with a session scoped request objec"
 @pytest.fixture
 def dir_meas_amount(tmp_path):
     dir_meas_amount = tmp_path
