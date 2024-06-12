d2e44b5717984803cc2d3aed69b817dba2357fc1
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-06-12 15:23:09 +0200

    fixed testcase

--- tests/test_data_retrieve.py
+++ tests/test_data_retrieve.py
@@ -132,10 +132,11 @@ def test_napcorrection_notdefined(df_meas_2010):
 
 @pytest.mark.unittest
 def test_clip_timeseries_physical_break(df_ext):
-    df_ext.attrs["station"] = "VLIELHVN"
-    df_ext_clipped = kw.data_retrieve.clip_timeseries_physical_break(df_meas=df_ext)
-    assert len(df_ext_clipped) != len(df_ext)
-    assert df_ext_clipped.index[0] >= pd.Timestamp("1933-01-01 00:00:00 +01:00")
+    df_ext_vlie = df_ext.copy() # only change attributes on a copy of the dataframe
+    df_ext_vlie.attrs["station"] = "VLIELHVN"
+    df_ext_vlie_clipped = kw.data_retrieve.clip_timeseries_physical_break(df_meas=df_ext_vlie)
+    assert len(df_ext_vlie_clipped) != len(df_ext_vlie)
+    assert df_ext_vlie_clipped.index[0] >= pd.Timestamp("1933-01-01 00:00:00 +01:00")
 
 
 @pytest.mark.unittest
