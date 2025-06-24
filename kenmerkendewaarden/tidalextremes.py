052af943a31c6148d0e8d94a05f498895fdac914
veenstrajelmer <veenstrajelmer@gmail.com>
2025-06-24 16:14:43 +0200

    deprecated kw.calc_hat_lat_fromcomponents()

--- kenmerkendewaarden/tidalextremes.py
+++ kenmerkendewaarden/tidalextremes.py
@@ -6,7 +6,6 @@ Created on Mon Jun  9 20:15:54 2025
 """
 
 import pandas as pd
-import datetime as dt
 import hatyan
 import logging
 from kenmerkendewaarden.utils import crop_timeseries_last_nyears, raise_empty_df
@@ -14,54 +13,10 @@ from kenmerkendewaarden.tidalindicators import calc_getijcomponenten
 from kenmerkendewaarden.slotgemiddelden import calc_slotgemiddelden
 
 __all__ = [
-    "calc_hat_lat_fromcomponents",
     "calc_hat_lat_frommeasurements",
 ]
 
 logger = logging.getLogger(__name__)
-
-
-def calc_hat_lat_fromcomponents(comp: pd.DataFrame) -> tuple:
-    """
-    Derive highest and lowest astronomical tide (HAT/LAT) from a component set.
-    The component set is used to make a tidal prediction for an arbitrary period of 19 years with a 10 minute interval.
-    The max/min values of the predictions of all years are the HAT/LAT values.
-    The HAT/LAT is very dependent on the A0 of the component set. Therefore, the HAT/LAT values are
-    relevant for the same year as the slotgemiddelde that is used to replace A0 in the component set.
-    For instance, if the slotgemiddelde is valid for 2021.0, HAT and LAT are also relevant for that year.
-    It is important to use the same tidal prediction settings as used to derive the tidal components.
-
-    Parameters
-    ----------
-    comp : pd.DataFrame
-        DataFrame with amplitudes and phases for a list of components.
-
-    Returns
-    -------
-    tuple
-        hat and lat values.
-
-    """
-    
-    min_vallist_allyears = pd.Series(dtype=float)
-    max_vallist_allyears = pd.Series(dtype=float)
-    tz = comp.attrs["tzone"] # to avoid tzone warning for all years
-    logger.info("generating prediction for 19 years")
-    # 19 arbitrary consequtive years to capture entire nodal cycle
-    for year in range(2020, 2039):  
-        times_pred_all = pd.date_range(
-            start=dt.datetime(year, 1, 1), end=dt.datetime(year + 1, 1, 1), freq="10min",
-            tz=tz,
-        )
-        ts_prediction = hatyan.prediction(comp=comp, times=times_pred_all)
-
-        min_vallist_allyears.loc[year] = ts_prediction["values"].min()
-        max_vallist_allyears.loc[year] = ts_prediction["values"].max()
-
-    logger.info("deriving hat/lat")
-    hat = max_vallist_allyears.max()
-    lat = min_vallist_allyears.min()
-    return hat, lat
 
 
 def predict_19y_peryear(comp, yearmax=2039):
