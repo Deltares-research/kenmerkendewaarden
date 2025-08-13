67a5b51fc09271e82e57dad9f365d0ccbb0685a8
veenstrajelmer <veenstrajelmer@gmail.com>
2025-08-13 15:44:48 +0200

    Bump version: 0.3.1 → 0.4.0

--- kenmerkendewaarden/__init__.py
+++ kenmerkendewaarden/__init__.py
@@ -4,7 +4,7 @@ Derive indicators from waterlevel measurements
 
 __author__ = """Jelmer Veenstra"""
 __email__ = "jelmer.veenstra@deltares.nl"
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 from kenmerkendewaarden.data_retrieve import *
 from kenmerkendewaarden.data_analysis import *
