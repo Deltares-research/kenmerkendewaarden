269f25380d6e0922e7756c727f3cd52962ec3df0
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2025-12-19 19:53:15 +0100

    267 use ddlpylocations cache (#269)
    * minimal ddlpy version
    
    * remove calls to retrieve_catalog() with deprecated overwrite argument
    
    * info logging for ddlpy
    
    * updated whatsnew

--- examples/KWK_getcheckdata.py
+++ examples/KWK_getcheckdata.py
@@ -5,15 +5,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.close('all')
 import kenmerkendewaarden as kw
-from kenmerkendewaarden.data_retrieve import retrieve_catalog
 
 # set logging level to INFO to get log messages
 import logging
 logging.basicConfig() # calling basicConfig is essential to set logging level for sub-modules
 logging.getLogger("kenmerkendewaarden").setLevel(level="INFO")
-
-# retrieve fresh DDL catalog
-retrieve_catalog(overwrite=False)
+logging.getLogger("ddlpy").setLevel(level="INFO")
 
 # TODO: overview of data improvements: https://github.com/Deltares-research/kenmerkendewaarden/issues/29
 # TODO: overview of data issues in https://github.com/Deltares-research/kenmerkendewaarden/issues/4
@@ -151,7 +148,7 @@ if plot_stations:
 
 
 
-### WRITE CSV WITH STATION CODE/X/Y/EPSG
+### WRITE CSV WITH STATION CODE/Lon/Lat/EPSG
 if write_stations_table:
     # TODO: consider making retrieve_catalog public
     from kenmerkendewaarden.data_retrieve import retrieve_catalog
